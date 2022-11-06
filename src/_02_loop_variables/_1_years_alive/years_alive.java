b8804937e47894d0f31b5ff8644edefdae5579af
arnav <arnav@192.168.1.239>
2022-11-06 11:19:16 -0800

    did a lot of stuff

---
+++ src/_02_loop_variables/_1_years_alive/years_alive.java
@@ -0,0 +1,12 @@
+package _02_loop_variables._1_years_alive;
+
+public class years_alive {
+public static void main(String[] args) {
+	int born = 2005;
+	
+	int now = 2022;
+	
+	for(int i = born; i < now; i++) {
+		System.out.println("I was alive in " + i);	}
+}
+}
--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -7,20 +7,21 @@ public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
 		
+		rob.setSpeed(10);
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
-		// 2. Make the robot draw a star shape. Hint: angle=144.
-
-		// 3. Set the length of each line in the star to 30.
-
-		/** THE CHALLENGE: **/
-		/*
-		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
-		 * 
-		 * Hint: The distance between stars is 50.
-		 */
-
+		rob.setX(25);
+		
+		for (int i = 0; i < 5; i++) {
+			rob.penDown();
+			rob.turn(144);
+			rob.move(30);
+		}
+		
+		rob.setX(100);
+	
 	}
 
+
 }
+
