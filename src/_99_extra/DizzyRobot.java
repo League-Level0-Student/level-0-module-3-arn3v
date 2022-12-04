d2c74800f2078e814e515dcfa661d933f232df7d
arnav <arnav@192.168.1.239>
2022-12-04 10:26:11 -0800

    nun

--- src/_99_extra/DizzyRobot.java
+++ src/_99_extra/DizzyRobot.java
@@ -8,7 +8,7 @@ import org.jointheleague.graphical.robot.Robot;
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
-		robot.setSpeed(50);
+		robot.setSpeed(10);
 		String userinput = JOptionPane.showInputDialog(null, "How many times would you like the Robot to spin?" ); 
 		dance(Integer.parseInt(userinput));
 				
